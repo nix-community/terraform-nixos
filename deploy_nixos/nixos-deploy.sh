11f35c7e054b8a910ece36ab49838254c2e760e2
zimbatm <zimbatm@zimbatm.com>
2020-11-22 14:07:11 +0100

    deploy_nixos: add ssh_private_key
    Sometimes it's useful to pass the SSH key content directly.

--- deploy_nixos/nixos-deploy.sh
+++ deploy_nixos/nixos-deploy.sh
@@ -11,7 +11,6 @@ buildArgs=(
 )
 profile=/nix/var/nix/profiles/system
 # will be set later
-controlPath=
 sshOpts=(
   -o "ControlMaster=auto"
   -o "ControlPersist=60"
@@ -32,7 +31,7 @@ outPath="$2"
 targetHost="$3"
 targetPort="$4"
 buildOnTarget="$5"
-sshPrivateKeyFile="$6"
+sshPrivateKey="$6"
 action="$7"
 shift 7
 
@@ -42,8 +41,13 @@ buildArgs+=("$@")
 
 sshOpts+=( -p "${targetPort}" )
 
-if [[ -n "${sshPrivateKeyFile}" && "${sshPrivateKeyFile}" != "-" ]]; then
-    sshOpts+=( -o "IdentityFile=${sshPrivateKeyFile}" )
+workDir=$(mktemp -d)
+trap 'rm -rf "$workDir"' EXIT
+
+if [[ -n "${sshPrivateKey}" ]]; then
+  sshPrivateKeyFile="$workDir/ssh_key"
+  echo "$sshPrivateKey" > "$sshPrivateKeyFile"
+  sshOpts+=( -o "IdentityFile=${sshPrivateKeyFile}" )
 fi
 
 ### Functions ###
@@ -62,6 +66,8 @@ targetHostCmd() {
   # `ssh` did not properly maintain the array nature of the command line,
   # erroneously splitting arguments with internal spaces, even when using `--`.
   # Tested with OpenSSH_7.9p1.
+  #
+  # shellcheck disable=SC2029
   ssh "${sshOpts[@]}" "$targetHost" "./maybe-sudo.sh ${*@Q}"
 }
 
@@ -69,9 +75,8 @@ targetHostCmd() {
 # operations by not re-creating SSH sessions between each command. At the end
 # of the run, the session is forcefully terminated.
 setupControlPath() {
-  controlPath=$(mktemp)
   sshOpts+=(
-    -o "ControlPath=$controlPath"
+    -o "ControlPath=$workDir/ssh_control"
   )
   cleanupControlPath() {
     local ret=$?
@@ -80,7 +85,7 @@ setupControlPath() {
     # Close ssh multiplex-master process gracefully
     log "closing persistent ssh-connection"
     ssh "${sshOpts[@]}" -O stop "$targetHost"
-    rm -f "$controlPath"
+    rm -rf "$workDir"
     exit "$ret"
   }
   trap cleanupControlPath EXIT
